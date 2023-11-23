189a314265a9267bdab65fa35384ed1c829a6d27
Sridhar Prasanth <111106030+sridharprasanthp@users.noreply.github.com>
2023-11-23 13:02:57 +0530

    redeemed_rewards

--- src/components/dashboard/projectStatus.jsx
+++ src/components/dashboard/projectStatus.jsx
@@ -16,8 +16,10 @@ export default function ProjectStatus() {
                     console.error('Error fetching :', error);
                 });
         }, []);
- let reward_name= redeeemed && redeeemed?.map((dat)=>dat?.results?.reward_id?.title)
- let reward_points=redeeemed && redeeemed?.map((dat)=>dat?.results?.reward_id?.points)
+ const reward_name= redeeemed && redeeemed?.map((dat)=>dat?.reward_id?.title)
+ const reward_points=redeeemed && redeeemed?.map((dat)=>dat?.reward_id?.points)
+
+ 
 
     return (
         <div className="card">
