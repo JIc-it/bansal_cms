dd54ef244b5ab3586ebdc73315bda6d6dee8fcf5
Sridhar Prasanth <111106030+sridharprasanthp@users.noreply.github.com>
2023-11-24 17:17:41 +0530

    Edit-Reward-Redeem-reward-dashboard-Architect

--- src/components/rewards/rewardProducts/editReward.jsx
+++ src/components/rewards/rewardProducts/editReward.jsx
@@ -13,27 +13,30 @@ const offcanvasStyle = {
     flexDirection: 'column',
 };
 
-export default function EditReward({ open, data, setOpen, refreshDataTable  }) {
+export default function EditReward({ open, data, setOpen, refreshDataTable, setisUpdated  }) {
     const [credentials, setCredentials] = useState({
         title: data.title,
         points: data.points,
         description: data.description,
         item_image: data.item_image,
         image_name: data.image_name,
+        is_active:data.is_active
     });
+const [showImages,setShowImages]=useState("")
 
+console.log(credentials.is_active,"isactive");
     const handleUpdate = async () => {
         try {
-            const { title, points, description, item_image, image_name } = credentials;
+            const { title, points, description, item_image, image_name,is_active } = credentials;
 
             const f_data = new FormData();
             f_data.append('title', title);
             f_data.append('points', points);
             f_data.append('description', description);
-            f_data.append('item_image', item_image);
-            f_data.append('is_active', true);
-            f_data.append('thumbnail_image', item_image);
-            f_data.append('image_name', image_name);
+            f_data.append('image', item_image);
+            f_data.append('is_active', is_active);
+            // f_data.append('thumbnail_image', item_image);
+            // f_data.append('image_name', image_name);
 
             const response = await editRewardProductRequest(data.id, f_data);
 
@@ -42,6 +45,7 @@ export default function EditReward({ open, data, setOpen, refreshDataTable  }) {
 
             if (response) {
                 toast.success("Reward product updated successfully!");
+                setisUpdated(true)
 
                 if (refreshDataTable && typeof refreshDataTable === 'function') {
                     refreshDataTable();
@@ -56,7 +60,7 @@ export default function EditReward({ open, data, setOpen, refreshDataTable  }) {
     };
 
     return (
-        <Offcanvas show={open} onHide={() => setOpen(false)} placement="end" style={{ overflow: 'auto' }}>
+        <Offcanvas show={open} onHide={() => {setOpen(false);setShowImages("")}} placement="end" style={{ overflow: 'auto' }}>
             <Offcanvas.Header style={{ marginLeft: 345 }} closeButton>
                 {/* <Offcanvas.Title>Reward Product Details</Offcanvas.Title> */}
             </Offcanvas.Header>
@@ -75,13 +79,24 @@ export default function EditReward({ open, data, setOpen, refreshDataTable  }) {
                         onChange={(e) => setCredentials({ ...credentials, description: e.target.value })} value={credentials.description} placeholder='description'></textarea>
                 </div>
                 <div style={{ marginTop: 20 }}>
-                    <img src={credentials.item_image} style={{ width: '50px', height: '50px' }} alt="Product Image" />
+                    <img src={showImages.trim()!==""?showImages:credentials.item_image} style={{ width: '50px', height: '50px' }} alt="Product Image" />
                 </div>
                 <div style={{ marginTop: 20, display: 'flex', justifyContent: 'space-between' }}>
                     <div>
                         <label>Product Photo (140*140)</label>
                         <input type="file" id="imageUpload" style={{ display: 'none' }}
-                            onChange={(e) => setCredentials({ ...credentials, item_image: e.target.files[0] })} />
+                            onChange={(e) => {setCredentials({ ...credentials, item_image: e.target.files[0] });
+                            const file = e.target.files[0];
+
+                            if (file) {
+                              const reader = new FileReader();
+                              reader.onloadend = () => {
+                                setShowImages(reader.result);
+                              };
+                             reader.readAsDataURL(file);
+                            }
+                            
+                            }} />
                         <label htmlFor="imageUpload" className="btn btn-secondary btn-sm" style={{ marginLeft: 106 }}>
                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M2 10C2 11.8856 2 12.8284 2.58579 13.4142C3.17157 14 4.11438 14 6 14H10C11.8856 14 12.8284 14 13.4142 13.4142C14 12.8284 14 11.8856 14 10" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
@@ -94,8 +109,10 @@ export default function EditReward({ open, data, setOpen, refreshDataTable  }) {
                 <div>
                     <h6>Status</h6><br />
                     <div className="form-check form-switch" style={{ position: 'relative', bottom: '35px', float: 'inline-end' }}>
-                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">In Active</label>
-                        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{credentials.is_active?"Active":"Inactive"}</label>
+                        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" 
+                        onChange={(e) => setCredentials({ ...credentials, is_active: !credentials.is_active })}
+                        defaultChecked={credentials.is_active} value={credentials.is_active}/>
                     </div>
                 </div>
             </div>
